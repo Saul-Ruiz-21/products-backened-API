5f6a56d4aa2b49c6d1580e458dbf1bb2e6d8494c
Saul Ruiz <saulruiz730@gmail.com>
2022-10-06 14:26:25 -0500

    created first view function

--- product/views.py
+++ product/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from urllib import response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from product import serializers
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def product_list(request):
+
+    if request.method == 'GET':
+        product = Product.objects.all()
+        serializer = ProductSerializer(product, many = True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
